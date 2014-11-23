3bba35c88c926381256914cd13f33072acbd829d
chocotan <loli@linux.com>
2014-11-23 22:50:45 +0800

    Some browser will send post request while changing to chrome mode

--- src/main/java/io/loli/box/controller/IndexAction.java
+++ src/main/java/io/loli/box/controller/IndexAction.java
@@ -2,6 +2,7 @@ package io.loli.box.controller;
 
 import javax.inject.Named;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
@@ -14,6 +15,7 @@ import org.glassfish.jersey.server.mvc.Viewable;
 public class IndexAction {
 
     @GET
+    @POST
     @Produces({ "text/html" })
     public Viewable index() {
         return new Viewable("/index", this);
