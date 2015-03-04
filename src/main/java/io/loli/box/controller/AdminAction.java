e67b6f645b155027c05b815d15cbb2276049714d
chocotan <loli@linux.com>
2015-03-04 21:43:18 +0800

    Use backbone.js for url hash changes

--- src/main/java/io/loli/box/controller/AdminAction.java
+++ src/main/java/io/loli/box/controller/AdminAction.java
@@ -39,136 +39,153 @@ import org.glassfish.jersey.server.JSONP;
 
 @Path("/admin")
 public class AdminAction {
-    /**
-     * Template engine to generate html
-     */
-    private static JetEngine engine = JetEngine.create();
+	/**
+	 * Template engine to generate html
+	 */
+	private static JetEngine engine = JetEngine.create();
 
-    private static StorageService ss = AbstractStorageService.getDefaultInstance();
+	/**
+	 * Default storageService
+	 */
+	private static StorageService ss = AbstractStorageService
+			.getDefaultInstance();
 
-    private static Comparator<File> fileComparator = new Comparator<File>() {
+	/**
+	 * File comparator to sort
+	 */
+	private static Comparator<File> fileComparator = new Comparator<File>() {
 
-        @Override
-        public int compare(File o1, File o2) {
-            try {
-                if (o1.lastModified() - o2.lastModified() == 0) {
-                    return 0;
-                }
-                return o1.lastModified() - o2.lastModified() > 0 ? -1 : 1;
-            } catch (Exception e) {
-                return -1;
-            }
-        }
+		@Override
+		public int compare(File o1, File o2) {
+			try {
+				if (o1.lastModified() - o2.lastModified() == 0) {
+					return 0;
+				}
+				return o1.lastModified() - o2.lastModified() > 0 ? -1 : 1;
+			} catch (Exception e) {
+				return -1;
+			}
+		}
 
-    };
+	};
 
-    @GET
-    @JSONP
-    @Path("list")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<FileBean> list(@QueryParam(value = "year") String year, @QueryParam(value = "month") String month,
-        @QueryParam(value = "day") String day, @Context HttpServletRequest request) {
+	@GET
+	@JSONP
+	@Path("list")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<FileBean> list(@QueryParam(value = "year") String year,
+			@QueryParam(value = "month") String month,
+			@QueryParam(value = "day") String day,
+			@Context HttpServletRequest request) {
 
-        Object obj = request.getSession().getAttribute("login");
+		Object obj = request.getSession().getAttribute("login");
 
-        // 已经登陆
-        if (obj != null && ((String) obj).equals("success")) {
-        } else {
-            // 没有登陆的话
-            return new ArrayList<FileBean>();
-        }
+		// 已经登陆
+		if (obj != null && ((String) obj).equals("success")) {
+		} else {
+			// 没有登陆的话
+			return new ArrayList<FileBean>();
+		}
 
-        List<File> files = ss.getFilesByDay(year, month, day);
-        Collections.sort(files, fileComparator);
-        return FileUtil.toFileBean(files);
-    }
+		List<File> files = ss.getFilesByDay(year, month, day);
+		Collections.sort(files, fileComparator);
+		return FileUtil.toFileBean(files);
+	}
 
-    @POST
-    @JSONP
-    @Path("delete")
-    @Consumes({ MediaType.APPLICATION_FORM_URLENCODED })
-    @Produces(MediaType.APPLICATION_JSON)
-    public StatusBean delete(@FormParam(value = "year") String year, @FormParam(value = "month") String month,
-        @FormParam(value = "day") String day, @FormParam(value = "name") String name,
-        @Context HttpServletRequest request) {
+	@POST
+	@JSONP
+	@Path("delete")
+	@Consumes({ MediaType.APPLICATION_FORM_URLENCODED })
+	@Produces(MediaType.APPLICATION_JSON)
+	public StatusBean delete(@FormParam(value = "year") String year,
+			@FormParam(value = "month") String month,
+			@FormParam(value = "day") String day,
+			@FormParam(value = "name") String name,
+			@Context HttpServletRequest request) {
 
-        Object obj = request.getSession().getAttribute("login");
+		Object obj = request.getSession().getAttribute("login");
 
-        // 已经登陆
-        if (obj != null && ((String) obj).equals("success")) {
-        } else {
-            // 没有登陆的话
-            return new StatusBean("error", "you must login");
-        }
+		// 已经登陆
+		if (obj != null && ((String) obj).equals("success")) {
+		} else {
+			// 没有登陆的话
+			return new StatusBean("error", "you must login");
+		}
 
-        try {
-            ss.deleteFile(year, month, day, name);
-        } catch (Exception e) {
-            return new StatusBean("error", e.getMessage());
-        }
-        return new StatusBean("success", "成功删除文件");
+		try {
+			ss.deleteFile(year, month, day, name);
+		} catch (Exception e) {
+			return new StatusBean("error", e.getMessage());
+		}
+		return new StatusBean("success", "成功删除文件");
 
-    }
+	}
 
-    @GET
-    @Path("/")
-    public Response index(@Context HttpServletRequest request) throws URISyntaxException {
+	@GET
+	@Path("/")
+	public Response index(@Context HttpServletRequest request)
+			throws URISyntaxException {
 
-        Object obj = request.getSession().getAttribute("login");
+		Object obj = request.getSession().getAttribute("login");
 
-        // 已经登陆
-        if (obj != null && ((String) obj).equals("success")) {
-            JetTemplate template = engine.getTemplate("/html/admin.html");
-            Map<String, Object> context = new HashMap<String, Object>();
-            StringWriter writer = new StringWriter();
-            template.render(context, writer);
-            return Response.ok(writer.toString(), "text/html;charset=UTF-8").build();
-        } else {
-            // 没有登陆的话
-            return Response.seeOther(new URI("/admin/login")).build();
-        }
+		// 已经登陆
+		if (obj != null && ((String) obj).equals("success")) {
+			JetTemplate template = engine.getTemplate("/html/admin.html");
+			Map<String, Object> context = new HashMap<String, Object>();
+			StringWriter writer = new StringWriter();
+			template.render(context, writer);
+			return Response.ok(writer.toString(), "text/html;charset=UTF-8")
+					.build();
+		} else {
+			// 没有登陆的话
+			return Response.seeOther(new URI("/admin/login")).build();
+		}
 
-    }
+	}
 
-    @GET
-    @Path("login")
-    public Response login(@Context HttpServletRequest request) throws MalformedURLException, URISyntaxException {
-        Object obj = request.getSession().getAttribute("login");
+	@GET
+	@Path("login")
+	public Response login(@Context HttpServletRequest request)
+			throws MalformedURLException, URISyntaxException {
+		Object obj = request.getSession().getAttribute("login");
 
-        // 已经登陆
-        if (obj != null && ((String) obj).equals("success")) {
-            return Response.seeOther(new URI("/admin/")).build();
-        }
+		// 已经登陆
+		if (obj != null && ((String) obj).equals("success")) {
+			return Response.seeOther(new URI("/admin/")).build();
+		}
 
-        JetTemplate template = engine.getTemplate("/html/login.html");
-        Map<String, Object> context = new HashMap<String, Object>();
-        context.put("email", LoliBoxConfig.getInstance().getEmail());
-        StringWriter writer = new StringWriter();
-        template.render(context, writer);
-        return Response.ok(writer.toString(), "text/html;charset=UTF-8").build();
-    }
+		JetTemplate template = engine.getTemplate("/html/login.html");
+		Map<String, Object> context = new HashMap<String, Object>();
+		context.put("email", LoliBoxConfig.getInstance().getEmail());
+		StringWriter writer = new StringWriter();
+		template.render(context, writer);
+		return Response.ok(writer.toString(), "text/html;charset=UTF-8")
+				.build();
+	}
 
-    @POST
-    @Path("login")
-    public Response loginSubmit(@FormParam(value = "password") @NotNull String password,
-        @Context HttpServletRequest request
+	@POST
+	@Path("login")
+	public Response loginSubmit(
+			@FormParam(value = "password") @NotNull String password,
+			@Context HttpServletRequest request
 
-    ) throws MalformedURLException, URISyntaxException {
-        if (LoliBoxConfig.getInstance().getPassword().equals(password)) {
-            // Login success
-            request.getSession().setAttribute("login", "success");
+	) throws MalformedURLException, URISyntaxException {
+		if (LoliBoxConfig.getInstance().getPassword().equals(password)) {
+			// Login success
+			request.getSession().setAttribute("login", "success");
 
-            return Response.seeOther(new URI("/admin/")).build();
-        } else {
-            // Login failed
-            request.getSession().removeAttribute("login");
+			return Response.seeOther(new URI("/admin/")).build();
+		} else {
+			// Login failed
+			request.getSession().removeAttribute("login");
 
-            JetTemplate template = engine.getTemplate("/html/login.html");
-            Map<String, Object> context = new HashMap<String, Object>();
-            StringWriter writer = new StringWriter();
-            context.put("error", "密码错误");
-            template.render(context, writer);
-            return Response.ok(writer.toString(), "text/html;charset=UTF-8").build();
-        }
-    }
+			JetTemplate template = engine.getTemplate("/html/login.html");
+			Map<String, Object> context = new HashMap<String, Object>();
+			StringWriter writer = new StringWriter();
+			context.put("error", "密码错误");
+			template.render(context, writer);
+			return Response.ok(writer.toString(), "text/html;charset=UTF-8")
+					.build();
+		}
+	}
 }
--- src/main/webapp/static/js/admin.js
+++ src/main/webapp/static/js/admin.js
@@ -1,44 +1,46 @@
 String.prototype.endsWith = function(suffix) {
-    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+	return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
 
 // 扩展Date的format方法
 Date.prototype.format = function(format) {
-    var o = {
-        "M+" : this.getMonth() + 1,
-        "d+" : this.getDate(),
-        "H+" : this.getHours(),
-        "m+" : this.getMinutes(),
-        "s+" : this.getSeconds(),
-        "q+" : Math.floor((this.getMonth() + 3) / 3),
-        "S" : this.getMilliseconds()
-    }
-    if (/(y+)/.test(format)) {
-        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    }
-    for ( var k in o) {
-        if (new RegExp("(" + k + ")").test(format)) {
-            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
-        }
-    }
-    return format;
+	var o = {
+		"M+" : this.getMonth() + 1,
+		"d+" : this.getDate(),
+		"H+" : this.getHours(),
+		"m+" : this.getMinutes(),
+		"s+" : this.getSeconds(),
+		"q+" : Math.floor((this.getMonth() + 3) / 3),
+		"S" : this.getMilliseconds()
+	}
+	if (/(y+)/.test(format)) {
+		format = format.replace(RegExp.$1, (this.getFullYear() + "")
+				.substr(4 - RegExp.$1.length));
+	}
+	for ( var k in o) {
+		if (new RegExp("(" + k + ")").test(format)) {
+			format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k]
+					: ("00" + o[k]).substr(("" + o[k]).length));
+		}
+	}
+	return format;
 }
 
 // Helper function that formats the file sizes
 function formatFileSize(bytes) {
-    if (typeof bytes !== 'number') {
-        return '';
-    }
+	if (typeof bytes !== 'number') {
+		return '';
+	}
 
-    if (bytes >= 1000000000) {
-        return (bytes / 1000000000).toFixed(2) + ' GB';
-    }
+	if (bytes >= 1000000000) {
+		return (bytes / 1000000000).toFixed(2) + ' GB';
+	}
 
-    if (bytes >= 1000000) {
-        return (bytes / 1000000).toFixed(2) + ' MB';
-    }
+	if (bytes >= 1000000) {
+		return (bytes / 1000000).toFixed(2) + ' MB';
+	}
 
-    return (bytes / 1000).toFixed(2) + ' KB';
+	return (bytes / 1000).toFixed(2) + ' KB';
 }
 
 var year = null;
@@ -46,214 +48,236 @@ var month = null;
 var day = null;
 
 $(document).ready(function() {
-    processHash();
+	var App = {
+		Models : {},
+		Routers : {},
+		Collections : {},
+		Views : {}
+	};
+
+	App.Routers.Main = Backbone.Router.extend({
+
+		// Hash maps for routes
+		routes : {
+			"" : "index",
+			"images/" : "index",
+			"images/:year" : "getImages",
+			"images/:year/:month" : "getImages",
+			"images/:year/:month/:day" : "getImages"
+		},
+
+		index : function() {
+			getImagesByDate();
+		},
+
+		getImages : function(year, month, day) {
+			getImagesByDate(year, month, day);
+		}
+
+	});
+	var router = new App.Routers.Main();
+	Backbone.history.start();
+
 });
 
 function fileSizeFormat(size) {
-    var kb = 1024;
-    var mb = 1024 * 1024;
-    if (size < 1024) {
-        return size.toFixed(2) + "B";
-    }
-    if (size < mb) {
-        return (size / kb).toFixed(2) + "KB";
-    }
-    if (size > mb) {
-        return (size / mb).toFixed(2) + "MB";
-    }
-    return "";
+	var kb = 1024;
+	var mb = 1024 * 1024;
+	if (size < 1024) {
+		return size.toFixed(2) + "B";
+	}
+	if (size < mb) {
+		return (size / kb).toFixed(2) + "KB";
+	}
+	if (size > mb) {
+		return (size / mb).toFixed(2) + "MB";
+	}
+	return "";
 
 }
 
 function fileDateFormat(date) {
-    return new Date(date).format("yyyy-MM-dd HH:mm:ss");
+	return new Date(date).format("yyyy-MM-dd HH:mm:ss");
 }
 
 function getFileLink(name) {
-    if (!name) {
-        if (year) {
-            if (month) {
-                if (day) {
-                    return year + "/" + month;
-                }
-                return year;
-            }
-            return "";
-        }
-        return "";
-    }
-    if (!year) {
-        return name;
-    } else if (!month) {
-        return year + "/" + name;
-    } else if (!day) {
-        return year + "/" + month + "/" + name;
-    }
+	if (!name) {
+		if (year) {
+			if (month) {
+				if (day) {
+					return year + "/" + month;
+				}
+				return year;
+			}
+			return "";
+		}
+		return "";
+	}
+	if (!year) {
+		return name;
+	} else if (!month) {
+		return year + "/" + name;
+	} else if (!day) {
+		return year + "/" + month + "/" + name;
+	}
 }
 
 function processResult(result) {
-    var size = result.length;
-    var html = "";
-    html += "<tr>";
-    html += "<td class='name-td'>" + "<a href='javascript:window.location.hash=\"#" + getFileLink()
-        + "\";processHash()'>" + ".." + "</a>" + "</td><td></td><td></td><td></td>";
-    html += "</tr>";
-    for (var i = 0; i < result.length; i++) {
-        html += "<tr>";
-        if (!result[i].file) {
-            // if is folder
-            html += "<td class='name-td folder-td'>" + "<a href='javascript:window.location.hash=\"#"
-                + getFileLink(result[i].name) + "\";processHash()'>" + result[i].name + "</a>" + "</td>";
-        } else {
-            // if is file
-            html += "<td class='name-td file-td'>" + "<a href='javascript:showImage(" + i + ")'>" + result[i].name
-                + "</a>" + "</td>";
-        }
-        html += "<td>" + (result[i].size == 0 ? "" : fileSizeFormat(result[i].size)) + "</td>";
-        html += "<td>" + fileDateFormat(result[i].lastModified) + "</td>";
-        html += "<td></td>";
-        html += "</tr>";
-    }
-    $("#file-list").html(html);
+	var size = result.length;
+	var html = "";
+	html += "<tr>";
+	html += "<td class='name-td'>" + "<a href='#/images/" + getFileLink()
+			+ "'>" + ".." + "</a>" + "</td><td></td><td></td><td></td>";
+	html += "</tr>";
+	for (var i = 0; i < result.length; i++) {
+		html += "<tr>";
+		if (!result[i].file) {
+			// if is folder
+			html += "<td class='name-td folder-td'>" + "<a href='#/images/"
+					+ getFileLink(result[i].name) + "'>" + result[i].name
+					+ "</a>" + "</td>";
+		} else {
+			// if is file
+			html += "<td class='name-td file-td'>"
+					+ "<a href='javascript:showImage(" + i + ")'>"
+					+ result[i].name + "</a>" + "</td>";
+		}
+		html += "<td>"
+				+ (result[i].size == 0 ? "" : fileSizeFormat(result[i].size))
+				+ "</td>";
+		html += "<td>" + fileDateFormat(result[i].lastModified) + "</td>";
+		html += "<td></td>";
+		html += "</tr>";
+	}
+	$("#file-list").html(html);
 }
 
 var currentId;
 
 function showImage(id) {
-    var imgObj = $("#file-list").find("tr").eq(id + 1);
-    var image = imgObj.find("td").eq(0).text();
-    $('#preview-modal').modal();
-    var path = "/images/" + year + "/" + month + "/" + day + "/" + image;
-    $("#preview-body").html("<img class='preview-img' src='" + path + "'>");
-    currentId = id;
+	var imgObj = $("#file-list").find("tr").eq(id + 1);
+	var image = imgObj.find("td").eq(0).text();
+	$('#preview-modal').modal();
+	var path = "/images/" + year + "/" + month + "/" + day + "/" + image;
+	$("#preview-body").html("<img class='preview-img' src='" + path + "'>");
+	currentId = id;
 }
 
-
 function nextImage() {
-    if (currentId + 1 > $("#file-list").find("tr").size()-2) {
-        $.notify("已经是最后一张图片了 !", "info");
-        return;
-    }
-    showImage(currentId + 1);
+	if (currentId + 1 > $("#file-list").find("tr").size() - 2) {
+		$.notify("已经是最后一张图片了 !", "info");
+		return;
+	}
+	showImage(currentId + 1);
 }
 
 function lastImage() {
-    if (currentId == 0) {
-        $.notify("已经是第一张图片了 !", "info");
-        return;
-    }
-    showImage(currentId - 1);
+	if (currentId == 0) {
+		$.notify("已经是第一张图片了 !", "info");
+		return;
+	}
+	showImage(currentId - 1);
 }
 
 function deleteImage() {
 
-    var imgObj = $("#file-list").find("tr").eq(currentId + 1);
-    var image = imgObj.find("td").eq(0).text();
+	var imgObj = $("#file-list").find("tr").eq(currentId + 1);
+	var image = imgObj.find("td").eq(0).text();
 
-    $.ajax({
-        url : '/admin/delete',
-        type : 'POST',
-        data : {
-            year : year,
-            month : month,
-            day : day,
-            name : image
-        },
-        contentType : "application/x-www-form-urlencoded",
-        async : true,
-        success : function(response, textStatus) {
-            if (response.status == "success") {
-                $.notify("删除成功", "success");
-                processHash(function() {
-                    currentId = currentId - 1;
-                    nextImage();
-                });
+	$.ajax({
+		url : '/admin/delete',
+		type : 'POST',
+		data : {
+			year : year,
+			month : month,
+			day : day,
+			name : image
+		},
+		contentType : "application/x-www-form-urlencoded",
+		async : true,
+		success : function(response, textStatus) {
+			if (response.status == "success") {
+				$.notify("删除成功", "success");
+				currentId = currentId - 1;
+				nextImage();
 
-            } else {
-                $.notify(response.message, "error");
-            }
-        },
-        error : function(XMLHttpRequest, textStatus, errorThrown) {
-        }
-    });
+			} else {
+				$.notify(response.message, "error");
+			}
+		},
+		error : function(XMLHttpRequest, textStatus, errorThrown) {
+		}
+	});
 }
 
-function processHash(func) {
-    var hash = window.location.hash;
-    if (hash) {
-        hash = hash.substring(1);
-        if (hash.indexOf("/") >= 0) {
-            var strs = hash.split("/");
-            if (strs[0]) {
-                year = strs[0];
-            } else {
-                year = null;
-                month = null;
-                day = null;
-            }
-            if (strs[1]) {
-                month = strs[1];
-            } else {
-                month = null;
-                day = null;
-            }
-            if (strs[2]) {
-                day = strs[2];
-            } else {
-                day = null;
-            }
+function getImagesByDate(myear, mmonth, mday) {
+	if (mday) {
+		if (year) {
+			year = myear;
+		} else {
+			year = null;
+			month = null;
+			day = null;
+		}
+		if (mmonth) {
+			month = month;
+		} else {
+			month = null;
+			day = null;
+		}
+		if (mday) {
+			day = mday;
+		} else {
+			day = null;
+		}
 
-        } else {
-            year = hash;
-            month = null;
-            day = null;
-        }
+	} else {
+		year = myear;
+		month = mmonth;
+		day = null;
+	}
 
-    } else {
-        year = null;
-        month = null;
-        day = null;
-    }
-    list(year, month, day, func);
+	list(myear, mmonth, mday);
 }
+
 function list(year, month, day, func) {
-    if (!year && !month && !day) {
-        $("#current-path").text("当前位置: /");
-        $.get("/admin/list", {}, function(result) {
-            processResult(result);
-            if (func)
-                func();
-        });
-    } else if (year && !month && !day) {
-        $("#current-path").text("当前位置: /" + year);
-        $.get("/admin/list", {
-            year : year
-        }, function(result) {
-            processResult(result);
-            if (func)
-                func();
-        });
-    } else if (year && month && !day) {
-        $("#current-path").text("当前位置: /" + year + "/" + month);
-        $.get("/admin/list", {
-            year : year,
-            month : month
-        }, function(result) {
-            processResult(result);
-            if (func)
-                func();
-        });
-    } else if (year && month && day) {
-        $("#current-path").text("当前位置: /" + year + "/" + month + "/" + day);
-        $.get("/admin/list", {
-            year : year,
-            month : month,
-            day : day
-        }, function(result) {
-            processResult(result);
-            if (func)
-                func();
-        });
-    }
+	if (!year && !month && !day) {
+		$("#current-path").text("当前位置: /");
+		$.get("/admin/list", {}, function(result) {
+			processResult(result);
+			if (func)
+				func();
+		});
+	} else if (year && !month && !day) {
+		$("#current-path").text("当前位置: /" + year);
+		$.get("/admin/list", {
+			year : year
+		}, function(result) {
+			processResult(result);
+			if (func)
+				func();
+		});
+	} else if (year && month && !day) {
+		$("#current-path").text("当前位置: /" + year + "/" + month);
+		$.get("/admin/list", {
+			year : year,
+			month : month
+		}, function(result) {
+			processResult(result);
+			if (func)
+				func();
+		});
+	} else if (year && month && day) {
+		$("#current-path").text("当前位置: /" + year + "/" + month + "/" + day);
+		$.get("/admin/list", {
+			year : year,
+			month : month,
+			day : day
+		}, function(result) {
+			processResult(result);
+			if (func)
+				func();
+		});
+	}
 
 }
