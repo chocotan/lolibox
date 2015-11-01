324015d4359d0f861bc1229a2b27d6a533ce183f
chocotan <loli@linux.com>
2015-11-01 23:03:07 +0800

    Update import

--- src/main/java/io/loli/box/SecurityConfig.java
+++ src/main/java/io/loli/box/SecurityConfig.java
@@ -5,13 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 /**
  * @author choco
  */
-@EnableWebMvcSecurity
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
