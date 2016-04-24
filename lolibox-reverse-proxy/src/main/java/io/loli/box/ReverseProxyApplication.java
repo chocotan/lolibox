c06ff038af100edcebecaadc13082304d07e09e8
chocotan <loli@linux.com>
2016-04-24 22:36:36 +0800

    Change packing to war

--- lolibox-reverse-proxy/src/main/java/io/loli/box/ReverseProxyApplication.java
+++ lolibox-reverse-proxy/src/main/java/io/loli/box/ReverseProxyApplication.java
@@ -2,11 +2,19 @@ package io.loli.box;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-public class ReverseProxyApplication {
+public class ReverseProxyApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ReverseProxyApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ReverseProxyApplication.class, args);
     }
--- lolibox-server/src/main/java/io/loli/box/LoliboxApplication.java
+++ lolibox-server/src/main/java/io/loli/box/LoliboxApplication.java
@@ -9,6 +9,8 @@ import org.hashids.Hashids;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -20,7 +22,12 @@ import org.springframework.social.config.annotation.EnableSocial;
 @EnableJpaRepositories
 @Import({MvcConfig.class, SecurityConfig.class})
 @EnableSocial
-public class LoliboxApplication {
+public class LoliboxApplication extends SpringBootServletInitializer{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LoliboxApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(LoliboxApplication.class, args);
     }
