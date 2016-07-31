fe8f53b3b79ad00a772032a06bf3d306b4386e3f
chocotan <loli@linux.com>
2016-07-31 15:46:17 +0800

    Add missing configuration

--- lolibox-server/src/main/java/io/loli/box/oauth2/ResourceServer.java
+++ lolibox-server/src/main/java/io/loli/box/oauth2/ResourceServer.java
@@ -1,11 +1,15 @@
 package io.loli.box.oauth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
+@EnableResourceServer
 public class ResourceServer extends ResourceServerConfigurerAdapter {
 
     @Override
@@ -14,11 +18,13 @@ public class ResourceServer extends ResourceServerConfigurerAdapter {
                 .authorizeRequests().anyRequest().access("#oauth2.hasScope('write')");
     }
 
+    @Autowired
+    private TokenStore tokenStore;
+
     @Override
     public void configure(ResourceServerSecurityConfigurer resources)
             throws Exception {
-
-        resources.resourceId("lolibox");
+        resources.tokenStore(tokenStore);
     }
 
 }
